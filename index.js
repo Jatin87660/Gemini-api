bc5669af501c0b737c4ff113d11e808a3a06b86a
Jatin87660 <jatinagrawal0945@gmail.com>
2025-08-02 00:33:39 +0530

    Revert "Your change description"

--- index.js
+++ index.js
@@ -98,15 +98,8 @@ app.post("/qa", async (req, res) => {
   }
 
   try {
-   const answers = await runQA(pdfUrl, questions);
-
-// Make sure answers contain actual newlines, not escaped \n or <br>
-const formattedAnswers = answers.map(ans =>
-  typeof ans === 'string' ? ans.replace(/\\n/g, '\n') : ans
-);
-
-res.json({ answers: formattedAnswers });
-
+    const answers = await runQA(pdfUrl, questions);
+    res.json({ answers });
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: "Failed to process request" });
